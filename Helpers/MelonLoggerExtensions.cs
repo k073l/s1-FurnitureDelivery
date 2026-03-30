473695fb8128dca38f84c694aa2d03ac6a1a0bd7
k0 <21180271+k073l@users.noreply.github.com>
2026-03-30 10:53:58 +0200

    build: update build system

--- Helpers/MelonLoggerExtensions.cs
+++ Helpers/MelonLoggerExtensions.cs
@@ -9,42 +9,7 @@ public static class MelonLoggerExtensions
 {
     public static void Debug(this MelonLogger.Instance logger, string message, bool stacktrace = true)
     {
-#if RELEASE
-        // Suppress debug logs in Release
-#else
-        var melon = MelonUtils.GetMelonFromStackTrace();
-        var namesection_color = MelonLogger.DefaultMelonColor;
-        if (melon is { Info: not null })
-            namesection_color = melon.ConsoleColor;
-
-        var name = GetLoggerName(logger);
-        var finalMessage = stacktrace
-            ? $"[DEBUG] {GetCallerInfo()} - {message}"
-            : $"[DEBUG] {message}";
-
-        InvokeNativeMsg(namesection_color, MelonLogger.DefaultTextColor, name, finalMessage);
-#endif
-    }
-
-    private static string GetLoggerName(MelonLogger.Instance logger)
-    {
-        var field = typeof(MelonLogger.Instance)
-            .GetField("Name", BindingFlags.NonPublic | BindingFlags.Instance);
-        return field?.GetValue(logger) as string;
-    }
-
-    private static void InvokeNativeMsg(Color namesectionColor, Color textColor, string nameSection, string message)
-    {
-        var method = typeof(MelonLogger)
-            .GetMethod("NativeMsg", BindingFlags.NonPublic | BindingFlags.Static);
-
-        method?.Invoke(null, new object[] {
-            namesectionColor,
-            textColor,
-            nameSection,
-            message ?? "null",
-            false // skipStackWalk
-        });
+        MelonDebug.Msg(stacktrace ? $"[{GetCallerInfo()}] {message}" : message);
     }
 
     private static string GetCallerInfo()
