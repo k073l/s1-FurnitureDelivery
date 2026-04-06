14c6fa7f6fc9ec386bb1d2d67971fb1066034e98
k0 <21180271+k073l@users.noreply.github.com>
2026-04-06 22:52:53 +0200

    fix: add UIPathConstants to VSC
    i have no idea how i missed that

---
+++ Shops/UI/UIPathConstants.cs
@@ -0,0 +1,35 @@
+namespace FurnitureDelivery.Shops.UI;
+
+public static class UIPathConstants
+{
+    public const string Container = "Container";
+    public const string Header = "Container/Header";
+    public const string Listings = "Listings";
+    public const string ListingContainer = "ListingContainer";
+    public const string Entries = "Entries";
+    public const string BackButton = "Container/BackButton";
+    public const string Confirm = "Container/Confirm";
+    public const string DestinationDropdown = "Container/DestinationDropdown";
+    public const string LoadingDockDropdown = "Container/LoadingDockDropdown";
+    public const string ItemTotal = "Container/ItemTotal/Amount";
+    public const string OrderTotal = "Container/OrderTotal/Amount";
+    public const string DeliveryFee = "Container/DeliveryFee/Amount";
+    public const string DeliveryTime = "Container/DeliveryTime";
+    public const string Dropshadow = "Dropshadow";
+    public const string Order = "Container/Contents/Container/Shops/Order";
+    public const string ScrollViewContent = "Container/Scroll View/Viewport/Content";
+
+    public const string IconImage = "Container/Icon/Image";
+    public const string Title = "Container/Labels/Container/Title";
+    public const string Description = "Container/Labels/Container/Description";
+
+    public const string HeaderFallback = "Header";
+    public const string BackButtonFallback = "BackButton";
+    public const string OrderButtonFallback = "OrderButton";
+    public const string DestinationDropdownFallback = "DestinationDropdown";
+    public const string LoadingDockDropdownFallback = "LoadingDockDropdown";
+    public const string DeliveryTimeLabelFallback = "DeliveryTimeLabel";
+    public const string ItemTotalLabelFallback = "ItemTotalLabel";
+    public const string OrderTotalLabelFallback = "OrderTotalLabel";
+    public const string DeliveryFeeLabelFallback = "DeliveryFeeLabel";
+}
